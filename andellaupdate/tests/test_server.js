b61adc82d2f8127a84bf48f6b0d5bf3cbd753c4e
Udobong <32451159+Udobong@users.noreply.github.com>
2017-12-15 18:16:27 +0100

    Update test_server.js

--- andellaupdate/tests/test_server.js
+++ andellaupdate/tests/test_server.js
@@ -1,8 +1,1 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
 
-var UserController = require('./user/UserController');
-app.use('/users', UserController);
-
-module.exports = app;
